93f9db9f484a8ec2da09b823a5a229918d21183b
victorarroyavecastaneda <victorarroyavecastaneda@gmail.com>
2024-06-06 21:01:31 +0000

    update login

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="w-screen h-screen flex flex-col items-center justify-center">
-    <h3 class="text-xl text-center text-white font-bold mb-10">Welcome to chat</h3>
+    <img src="https://firebasestorage.googleapis.com/v0/b/assets-3ce98.appspot.com/o/login.png?alt=media&token=afed9e4e-e351-4b7c-b349-7ed2770f2d8c" alt="login image" class="drop-shadow-purple mb-10">
     <label class="input input-bordered flex items-center gap-2 mb-8">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70">
         <path
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{vue,html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        'purple': '5px 5px 10px #d9c6f4'
+      }
+    },
   },
   plugins: [
     require('daisyui'),
